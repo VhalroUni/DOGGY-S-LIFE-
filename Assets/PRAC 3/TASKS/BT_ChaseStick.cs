ec309cd64c457cae53332c4e82fed65d2b5c9497
VhalroUni <avalerof@edu.tecnocampus.cat>
2026-03-10 16:41:28 +0100

    Stick Thrown

---
+++ Assets/PRAC 3/TASKS/BT_ChaseStick.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using BTs;
+
+[CreateAssetMenu(fileName = "BT_ChaseStick", menuName = "Behaviour Trees/BT_ChaseStick", order = 1)]
+public class BT_ChaseStick : BehaviourTree
+{
+    /* If necessary declare BT parameters here. 
+       All public parameters must be of type string. All public parameters must be
+       regarded as keys in/for the blackboard context.
+       Use prefix "key" for input parameters (information stored in the blackboard that must be retrieved)
+       use prefix "keyout" for output parameters (information that must be stored in the blackboard)
+
+       e.g.
+       public string keyDistance;
+       public string keyoutObject 
+
+       NOTICE: BT's with parameters cannot be constructed using ScriptableObject.CreateInstance<>
+       An explicit constructor with new must be used. Unity will complain...
+       Whenever possible, use parameter-less BT's. Use blackboard to pass information.
+       TOP-level BTs (those attached to the executor) cannot have parameters
+       
+       In future versions, BT parameters may cease to exit
+
+     */
+
+     // construtor
+    public BT_ChaseStick()  { 
+        /* Receive BT parameters and set them. Remember all are of type string */
+    }
+    
+    public override void OnConstruction()
+    {
+        /* Write here (method OnConstruction) the code that constructs the Behaviour Tree 
+           Remember to set the root attribute to a proper node
+           e.g.
+            ...
+            root = new Sequence();
+            ...
+
+          A behaviour tree can use other behaviour trees.  
+      */
+
+        root = new Sequence();
+
+        root.AddChild(new ACTION_IncreaseSpeed());
+        root.AddChild(new ACTION_Arrive("theStick"));
+        root.AddChild(new ACTION_Take("theStick"));
+        root.AddChild(new ACTION_Arrive("stickAbandonArea"));
+        root.AddChild(new ACTION_Drop("theStick"));
+        root.AddChild(new ACTION_DecreaseSpeed());
+        root.AddChild(new ACTION_SetTag());
+    }
+}
