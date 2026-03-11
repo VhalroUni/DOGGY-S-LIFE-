c03d8df4859fa06fd6c40714f8b3dd8e10cd2ed1
Nef <165885331+OscarGP52@users.noreply.github.com>
2026-03-11 11:45:08 +0100

    LAST COMMIT

---
+++ Assets/PRAC 3/BTs/BT_DogLife.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using BTs;
+
+[CreateAssetMenu(fileName = "BT_DogLife", menuName = "Behaviour Trees/BT_DogLife", order = 1)]
+public class BT_DogLife : BehaviourTree
+{
+    /* If necessary declare BT parameters here. 
+       All public parameters must be of type string. All public parameters must be
+       regarded as keys in/for the blackboard context.
+       Use prefix "key" for input parameters (information stored in the blackboard that must be retrieved)
+       use prefix "keyout" for output parameters (information that must be stored in the blackboard)
+
+       e.g.
+       public string keyDistance;
+       public string keyoutObject 
+
+       NOTICE: BT's with parameters cannot be constructed using ScriptableObject.CreateInstance<>
+       An explicit constructor with new must be used. Unity will complain...
+       Whenever possible, use parameter-less BT's. Use blackboard to pass information.
+       TOP-level BTs (those attached to the executor) cannot have parameters
+       
+       In future versions, BT parameters may cease to exit
+
+     */
+
+     // construtor
+    public BT_DogLife()  { 
+        /* Receive BT parameters and set them. Remember all are of type string */
+    }
+    
+    public override void OnConstruction()
+    {
+        /* Write here (method OnConstruction) the code that constructs the Behaviour Tree 
+           Remember to set the root attribute to a proper node
+           e.g.
+            ...
+            root = new Sequence();
+            ...
+
+          A behaviour tree can use other behaviour trees.  
+      */
+        DynamicSelector dogLife = new DynamicSelector();
+
+        dogLife.AddChild(new CONDITION_InstanceNear("1000", "BULLY"), 
+            new ACTION_Hide()
+        );
+
+        dogLife.AddChild(new CONDITION_IsHungry(),
+            new BT_Eat()
+        );
+
+        dogLife.AddChild(new CONDITION_InstanceNear("1000", "STICK", "true", "TheStick"),
+            new BT_ChaseStick()
+        );
+
+        dogLife.AddChild(new CONDITION_AlwaysTrue(),
+            new BT_RelaxedWalk()
+        );
+
+        root = new RepeatForeverDecorator(dogLife);
+
+    }
+}
